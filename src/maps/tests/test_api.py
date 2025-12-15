63e0940de5d8e60de83eed9757fb706694469f08
Maxence HAOUARI <maxence.haouari@beta.gouv.fr>
2025-12-15 13:27:39 +0100

    fix(tests):  test_map_api_export

--- src/maps/tests/test_api.py
+++ src/maps/tests/test_api.py
@@ -257,7 +257,7 @@ def test_map_api_export(verified_api):
     assert res.status_code == 200
     assert (
         res.content
-        == b"siret,nom_etablissement,adresse_td,profiles,bsdd_roles,bsdnd_roles,bsda_roles,bsff_roles,bsdasri_roles,bsvhu_roles,texs_dd_roles,dnd_roles,texs_roles,processing_operations\r\n00035182679548,Mycompany,,,,,,,,,,,,\r\n"
+        == b"siret,nom_etablissement,adresse_td,profiles,collector_profiles,installation_profiles,vhu_installation_profiles,bsdd_roles,bsdnd_roles,bsda_roles,bsff_roles,bsdasri_roles,bsvhu_roles,texs_dd_roles,dnd_roles,texs_roles,processing_operations\r\n00035182679548,Mycompany,,,,,,,,,,,,,,,\r\n"
     )
 
 
