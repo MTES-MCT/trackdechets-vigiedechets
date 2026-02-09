9f4f3edd72b4016f5231e44db9bb4621fc448698
Maxence HAOUARI <maxence.haouari@beta.gouv.fr>
2026-02-09 15:47:53 +0100

    feat(registry): add admin interfaces for SIREN and SIRET exports
    - Registered `RegistryV2ExportSirenAdmin` and `RegistryV2ExportSiretAdmin` classes for managing SIREN and SIRET exports in the Django admin interface.
    - Implemented inline admin for `RegistryV2ExportSiret` to display related SIRET exports within the SIREN export view.
    - Enhanced list display, filtering, and search capabilities for both SIREN and SIRET exports, improving usability in the admin panel.
    - Added actions to refresh registry states for selected exports, streamlining export management.

--- src/registry/admin.py
+++ src/registry/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import RegistryDownload, RegistryV2Export
+from .models import RegistryDownload, RegistryV2Export, RegistryV2ExportSiren, RegistryV2ExportSiret
 from .task import process_export
 
 
@@ -36,3 +36,60 @@ class RegistryV2ExportAdmin(admin.ModelAdmin):
     def action_update_export(self, request, queryset):
         for obj in queryset:
             process_export(obj.pk)
+
+
+class RegistryV2ExportSiretInline(admin.TabularInline):
+    """Inline admin for viewing child SIRET exports"""
+    model = RegistryV2ExportSiret
+    extra = 0
+    readonly_fields = ["id", "siret", "state", "registry_export_id", "created_at"]
+    fields = ["siret", "state", "registry_export_id", "created_at"]
+    can_delete = False
+    show_change_link = True
+
+
+@admin.register(RegistryV2ExportSiren)
+class RegistryV2ExportSirenAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "siren",
+        "state",
+        "total_sirets",
+        "completed_sirets",
+        "failed_sirets",
+        "export_format",
+        "created_at",
+        "registry_type",
+        "start_date",
+        "end_date",
+        "created_by_email",
+    ]
+    list_filter = ["registry_type", "export_format", "state"]
+    search_fields = ["siren", "created_by_email"]
+    list_select_related = ["created_by"]
+    readonly_fields = ["total_sirets", "completed_sirets", "failed_sirets"]
+    inlines = [RegistryV2ExportSiretInline]
+    actions = [
+        "action_update_export",
+    ]
+
+    @admin.action(description="Refresh registry states by checking Trackdéchets api")
+    def action_update_export(self, request, queryset):
+        for obj in queryset:
+            process_export(obj.pk)
+
+
+@admin.register(RegistryV2ExportSiret)
+class RegistryV2ExportSiretAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "siret",
+        "parent",
+        "state",
+        "registry_export_id",
+        "created_at",
+    ]
+    list_filter = ["state", "parent"]
+    search_fields = ["siret", "parent__siren"]
+    list_select_related = ["parent"]
+    readonly_fields = ["parent", "siret", "state", "registry_export_id", "created_at"]
