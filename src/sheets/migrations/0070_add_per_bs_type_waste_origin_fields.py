7c6dc76e3968c4d65e0982d2a7f9cff19fe990fb
Maxence HAOUARI <maxence.haouari@beta.gouv.fr>
2026-02-12 15:27:54 +0100

    feat(migrations): merge migration files

--- src/sheets/migrations/0070_add_per_bs_type_waste_origin_fields.py
+++ src/sheets/migrations/0070_add_per_bs_type_waste_origin_fields.py
@@ -4,76 +4,46 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sheets', '0069_remove_waste_origin_map_fields'),
+        ("sheets", "0069_remove_waste_origin_map_fields"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='computedinspectiondata',
-            name='waste_origin_data',
+            model_name="computedinspectiondata",
+            name="waste_origin_data",
         ),
         migrations.RemoveField(
-            model_name='computedinspectiondata',
-            name='waste_origin_graph',
+            model_name="computedinspectiondata",
+            name="waste_origin_graph",
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdd_waste_origin_data',
+            model_name="computedinspectiondata",
+            name="dangerous_waste_origin_data",
             field=models.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdd_waste_origin_graph',
+            model_name="computedinspectiondata",
+            name="dangerous_waste_origin_graph",
             field=models.TextField(blank=True),
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdd_non_dangerous_waste_origin_data',
+            model_name="computedinspectiondata",
+            name="non_dangerous_waste_origin_data",
             field=models.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdd_non_dangerous_waste_origin_graph',
+            model_name="computedinspectiondata",
+            name="non_dangerous_waste_origin_graph",
             field=models.TextField(blank=True),
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsda_waste_origin_data',
+            model_name="computedinspectiondata",
+            name="amiante_waste_origin_data",
             field=models.JSONField(default=dict),
         ),
         migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsda_waste_origin_graph',
-            field=models.TextField(blank=True),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdasri_waste_origin_data',
-            field=models.JSONField(default=dict),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsdasri_waste_origin_graph',
-            field=models.TextField(blank=True),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsff_waste_origin_data',
-            field=models.JSONField(default=dict),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsff_waste_origin_graph',
-            field=models.TextField(blank=True),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsvhu_waste_origin_data',
-            field=models.JSONField(default=dict),
-        ),
-        migrations.AddField(
-            model_name='computedinspectiondata',
-            name='bsvhu_waste_origin_graph',
+            model_name="computedinspectiondata",
+            name="amiante_waste_origin_graph",
             field=models.TextField(blank=True),
         ),
     ]
