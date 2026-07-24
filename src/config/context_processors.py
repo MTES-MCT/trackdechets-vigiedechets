321b05f54fa9e8d0e71afa58424282e478d50fcc
Florian <florian.blain@atimic.fr>
2026-07-24 12:35:25 +0200

    Update

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -1,12 +1,12 @@
-from django.conf import settings
+from .template_settings import get_template_runtime_settings
 
 
 def settings_processor(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {
-        "GUN_DATA_UPDATE_DATE_STRING": settings.GUN_DATA_UPDATE_DATE_STRING,
-        "GISTRID_DATA_UPDATE_DATE_STRING": settings.GISTRID_DATA_UPDATE_DATE_STRING,
-        "RNDTS_DATA_UPDATE_DATE_STRING": settings.RNDTS_DATA_UPDATE_DATE_STRING,
-        "MATOMO_SITE_ID": settings.MATOMO_SITE_ID,
-        "MATOMO_URL": settings.MATOMO_URL,
+        "GUN_DATA_UPDATE_DATE_STRING": get_template_runtime_settings().get("GUN_DATA_UPDATE_DATE_STRING", ""),
+        "GISTRID_DATA_UPDATE_DATE_STRING": get_template_runtime_settings().get("GISTRID_DATA_UPDATE_DATE_STRING", ""),
+        "RNDTS_DATA_UPDATE_DATE_STRING": get_template_runtime_settings().get("RNDTS_DATA_UPDATE_DATE_STRING", ""),
+        "MATOMO_SITE_ID": get_template_runtime_settings().get("MATOMO_SITE_ID", ""),
+        "MATOMO_URL": get_template_runtime_settings().get("MATOMO_URL", ""),
     }
